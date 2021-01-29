10c0eda7ce6c4880fc26ca789240b77df300e33f
mattbrios@gmail.com <mattbrios@gmail.com>
2021-01-29 19:32:03 -0300

    commit inicial

---
+++ src/components/Footer/index.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+// src/components/Footer/index.js
+const FooterWrapper = styled.footer`
+  background-color: #00000070;
+  padding: 20px;
+  display: flex;
+  align-items: center;
+  border-radius: 4px; 
+  img {
+    width: 58px;
+    margin-right: 23px;
+  }
+  a {
+    color: white;
+    text-decoration: none;
+    transition: .3s;
+    &:hover,
+    &:focus {
+      opacity: .5;
+    }
+    span {
+      text-decoration: underline;
+    }
+  }
+  span {
+    opacity: 0.5;
+    font-size: 12px;
+  }
+`;
+
+export default function Footer(props) {
+  return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <FooterWrapper {...props}>
+      <a href="https://mateusrios.com.br/">
+        <img src="https://mateusrios.com.br/assets/mr.png" alt="Logo Mateus Rios" />
+      </a>
+      <p>Mateus Rios<br/><span>Todos os direitos reservados</span></p>
+    </FooterWrapper>
+  );
+}
